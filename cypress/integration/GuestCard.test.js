74ec287065c69d533d6b7f1c599ad79db5b3ec2a
obenari <Omri_BenAri@intuit.com>
2019-06-02 13:52:11 +0300

    fitst test

--- cypress/integration/GuestCard.test.js
+++ cypress/integration/GuestCard.test.js
@@ -1,5 +1,5 @@
 /// <reference types="Cypress" />
-const url = ''; // url to an indevidual invoice will be provided to each participate
+const url = ''; // url to an individual invoice will be provided to each participate
 
 it('load page', () => {
     cy.visit(url)
