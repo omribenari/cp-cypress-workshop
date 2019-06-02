0be81929d11c12311d1ab6e27f196839f2d1ec33
obenari <Omri_BenAri@intuit.com>
2019-06-02 18:02:03 +0300

    move visit into a beforeEace close

--- cypress/integration/GuestCard.test.js
+++ cypress/integration/GuestCard.test.js
@@ -1,16 +1,19 @@
 /// <reference types="Cypress" />
 
-describe('Guest card tests', () => {
-  // url to an individual invoice will be provided to each developer
-  const url = 'https://connect.e2e.intuit.com/portal/app/CommerceNetwork/view/50e78fb1bbd14641babf5eb37926d9f41a27783a2a0c42c7ba638f2c5cd15b79ea52b182aa714e20a4c9fd0acc762629?cta=viewinvoicenow&locale=en_US';
+// url to an individual invoice will be provided to each developer
+const url =
+  'https://connect.e2e.intuit.com/portal/app/CommerceNetwork/view/50e78fb1bbd14641babf5eb37926d9f41a27783a2a0c42c7ba638f2c5cd15b79ea52b182aa714e20a4c9fd0acc762629?cta=viewinvoicenow&locale=en_US';
 
-  it('load page', () => {
+describe('Guest card tests', () => {
+  beforeEach(() => {
     cy.visit(url);
+  });
+  
+  it('load page', () => {
     cy.contains('button', 'Pay');
   });
 
   it('try to pay with empty form', () => {
-    cy.visit(url);
     cy.contains('button', 'Pay').click();
     cy.get('.field-error-message-wrapper').should('have.length', 5);
   });
