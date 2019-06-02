70f601d3537dea103fc3db1ffd0f3b25e04b8e94
obenari <Omri_BenAri@intuit.com>
2019-06-02 17:37:51 +0300

    SAVEPOINT

--- cypress/integration/GuestCard.test.js
+++ cypress/integration/GuestCard.test.js
@@ -1,9 +1,17 @@
 /// <reference types="Cypress" />
 
-// url to an individual invoice will be provided to each developer
-const url = '';
+describe('Guest card tests', () => {
+  // url to an individual invoice will be provided to each developer
+  const url = 'https://connect.e2e.intuit.com/portal/app/CommerceNetwork/view/50e78fb1bbd14641babf5eb37926d9f41a27783a2a0c42c7ba638f2c5cd15b79ea52b182aa714e20a4c9fd0acc762629?cta=viewinvoicenow&locale=en_US';
 
-it('load page', () => {
+  it('load page', () => {
     cy.visit(url);
     cy.contains('button', 'Pay');
+  });
+
+  it('try to pay with empty form', () => {
+    cy.visit(url);
+    cy.contains('button', 'Pay').click();
+    cy.get('.field-error-message-wrapper').should('have.length', 5);
+  });
 });
