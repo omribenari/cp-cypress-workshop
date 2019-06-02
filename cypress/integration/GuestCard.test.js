d057e61889439bdd1b8b7ceb8a57789405f680ce
obenari <Omri_BenAri@intuit.com>
2019-06-02 18:38:32 +0300

    SAVEPOINT

--- cypress/integration/GuestCard.test.js
+++ cypress/integration/GuestCard.test.js
@@ -1,20 +1,44 @@
 /// <reference types="Cypress" />
 
-// url to an individual invoice will be provided to each developer
 const token =
   '50e78fb1bbd14641babf5eb37926d9f41a27783a2a0c42c7ba638f2c5cd15b79ea52b182aa714e20a4c9fd0acc762629';
+const amountToPay = '1';
+const card = {
+  nameOnCard: 'Omri Ben Ari',
+  cardNumber: '4111111111111111',
+  expDate: '12/23',
+  cvvCode: '123',
+  postalCode: '12345'
+};
 
 describe('Guest card tests', () => {
   beforeEach(() => {
     cy.visit(token);
   });
 
-  it('load page', () => {
+  it('Load page', () => {
     cy.contains('button', 'Pay');
   });
 
-  it('try to pay with empty form', () => {
+  it('Try to pay with empty form', () => {
     cy.contains('button', 'Pay').click();
     cy.get('.field-error-message-wrapper').should('have.length', 5);
+  });
+
+  it.only('Pay 1$ with a card', () => {
+    cy.get('.payment-amount-input-wrapper').within(() => {
+      cy.get('label.display-amount').click();
+      cy.get('input')
+        .clear()
+        .type(amountToPay);
+    });
+
+    cy.get('input[name="name"]').type(card.nameOnCard);
+    cy.get('input[name="number"]').type(card.cardNumber);
+    cy.get('input[name="expDate"]').type(card.expDate);
+    cy.get('input[name="cvc"]').type(card.cvvCode);
+    cy.get('input[name="zipCode"]').type(card.postalCode);
+
+    cy.get('.cpButton.pay').click();
   });
 });
