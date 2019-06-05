01ba55cc6e063122688f2d27aaa7bddcb392475f
obenari <Omri_BenAri@intuit.com>
2019-06-05 11:11:45 +0300

    create a login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,29 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const loginUrl = 'https://accounts-e2e.intuit.com/access_client/sign_in';
+
+const user = {
+  email: 'cypress1.iamtestpass@gmail.com',
+  password: 'Password1!'
+};
+
+Cypress.Commands.add('login', (usr = user.email, pw = user.password) => {
+  return cy
+    .request({
+      method: 'POST',
+      url: loginUrl,
+      body: {
+        username: usr,
+        password: pw,
+        namespaceId: '50000003'
+      }
+    })
+    .then(response => {
+      if (response.status !== 200) {
+        cy.log('An error occurred while trying to sign in to qb');
+      }
+      return response;
+    });
+});
