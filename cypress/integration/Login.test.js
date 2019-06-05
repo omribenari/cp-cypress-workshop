1775b758474227c9ed2fa6bc616184bfd80462ae
obenari <Omri_BenAri@intuit.com>
2019-06-05 10:56:37 +0300

    add login tests

--- cypress/integration/Login.test.js
+++ cypress/integration/Login.test.js
@@ -2,8 +2,28 @@
 
 const token =
   '50e78fb1bbd14641babf5eb37926d9f41a27783a2a0c42c7ba638f2c5cd15b79ea52b182aa714e20a4c9fd0acc762629';
+const loginUrl = 'https://accounts-e2e.intuit.com/access_client/sign_in';
 
-const email = 'cypress1.iamtestpass@gmail.com';
+const user = {
+  email: 'cypress1.iamtestpass@gmail.com',
+  password: 'Password1!'
+};
+
+const login = () => {
+  cy.request({
+    method: 'POST',
+    url: loginUrl,
+    body: {
+      username: user.email,
+      password: user.password,
+      namespaceId: '50000003'
+    }
+  }).then(response => {
+    if (response.status !== 200) {
+      cy.log('An error occurred while trying to sign in to qb');
+    }
+  });
+};
 
 describe('Login tests', () => {
   it('should contain sign-in button', () => {
@@ -12,16 +32,16 @@ describe('Login tests', () => {
   });
 
   it('should be signed in', () => {
-    cy.login();
+    login();
     cy.visit(token);
-    cy.contains('header', email);
+    cy.contains('header', user.email);
   });
 
   it.only('should signed out', () => {
-    cy.login();
+    login();
     cy.visit(token);
     cy.get('header')
-      .contains(email)
+      .contains(user.email)
       .click();
     cy.get('.sign-out-menu')
       .contains('Sign out')
