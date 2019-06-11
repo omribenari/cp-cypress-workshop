74424f82917de50b6df1c29e3a0a9d9b904b697f
obenari <Omri_BenAri@intuit.com>
2019-06-11 18:17:31 +0300

    Add pdf network stubbing tests

--- cypress/actions/InvoiceSummaryActions.js
+++ cypress/actions/InvoiceSummaryActions.js
@@ -1,5 +1,46 @@
 export const InvoiceSummaryActions = {
+  setRoutes() {
+    cy.server();
+    cy.route('/icnportal-server/rest/document/*.pdf').as('pdf');
+    cy.route('/icnportal-server/rest/document/*/image?pgNo=1').as('pdfPreview');
+  },
+  setRoutesNoPdfImg(responseStatus = 404) {
+    cy.server();
+    cy.route('/icnportal-server/rest/document/*.pdf').as('pdf');
+    cy.route({
+      method: 'GET',
+      url: '/icnportal-server/rest/document/*/image?pgNo=1',
+      status: responseStatus,
+      response: {}
+    }).as('pdfPreview');
+  },
+  waitForPdfAndPdfImage() {
+    cy.wait(['@pdf', '@pdfPreview']);
+  },
+  waitForPdfAndPdfImageRetries() {
+    cy.wait(['@pdf', '@pdfPreview', '@pdfPreview', '@pdfPreview']);
+  },
   getSummaryElement() {
     return cy.get('.pdf-preview-wrapper > .i-s > .flex-column');
+  },
+  getPdfPreviewElement() {
+    return cy.get('.pdf-preview-thumbnail-wrapper');
+  },
+  getPdfPreviewShimmer() {
+    this.getPdfPreviewElement().within(() => {
+      return cy.get('.shimmer');
+    });
+  },
+  getPdfPreviewImage() {
+    this.getPdfPreviewElement().within(() => {
+      return cy.get('img[alt="PDF Preview"]');
+    });
+  },
+  verifyPdfError() {
+    this.getPdfPreviewElement().within(() => {
+      cy.get('.error').as('error');
+      cy.get('@error').contains('Something went wrong');
+      cy.get('@error').contains('button', 'Try again');
+    });
   }
 };
--- cypress/integration/InvoiceSummary.test.js
+++ cypress/integration/InvoiceSummary.test.js
@@ -5,41 +5,70 @@ const token =
   '50e78fb1bbd14641babf5eb37926d9f41a27783a2a0c42c7ba638f2c5cd15b79ea52b182aa714e20a4c9fd0acc762629';
 
 describe('Invoice summary tests', () => {
-  beforeEach(() => {
-    cy.visit(token);
+  describe('standard network', () => {
+    beforeEach(() => {
+      actions.setRoutes();
+      cy.visit(token);
+    });
+
+    it('should contain invoice info', () => {
+      actions.getSummaryElement().contains('Invoice');
+      actions.getSummaryElement().contains('Due date');
+      actions.getSummaryElement().contains('Invoice total');
+
+      cy.getReduxStore().then(store => {
+        const {
+          invoice: {
+            invoiceNumber,
+            currency,
+            invoiceAmount,
+            isPartiallyPaid,
+            isFullyPaid,
+            invoiceDueDate
+          }
+        } = store;
+
+        actions.getSummaryElement().contains(invoiceNumber);
+
+        const expectedAmount = formatCurrencyNumber(currency, invoiceAmount);
+        actions.getSummaryElement().contains(expectedAmount);
+
+        if (isPartiallyPaid) {
+          actions.getSummaryElement().contains('Partially paid');
+        }
+        if (isFullyPaid) {
+          actions.getSummaryElement().contains('Fully paid');
+        }
+
+        const expectedDate = formatDate(invoiceDueDate);
+        actions.getSummaryElement().contains(expectedDate);
+      });
+    });
+
+    it('should show pdf shimmer', () => {
+      actions.getPdfPreviewShimmer();
+    });
+
+    it('should show pdf img', () => {
+      actions.waitForPdfAndPdfImage();
+      actions.getPdfPreviewImage();
+    });
   });
 
-  it('should contain invoice info', () => {
-    actions.getSummaryElement().contains('Invoice');
-    actions.getSummaryElement().contains('Due date');
-    actions.getSummaryElement().contains('Invoice total');
+  describe('network errors handling', () => {
+    it('fail to get pdf image - return 404', () => {
+      actions.setRoutesNoPdfImg();
+      cy.visit(token);
+      actions.waitForPdfAndPdfImage();
+      actions.verifyPdfError();
+    });
 
-    cy.getReduxStore().then(store => {
-      const {
-        invoice: {
-          invoiceNumber,
-          currency,
-          invoiceAmount,
-          isPartiallyPaid,
-          isFullyPaid,
-          invoiceDueDate
-        }
-      } = store;
-
-      actions.getSummaryElement().contains(invoiceNumber);
-
-      const expectedAmount = formatCurrencyNumber(currency, invoiceAmount);
-      actions.getSummaryElement().contains(expectedAmount);
-
-      if (isPartiallyPaid) {
-        actions.getSummaryElement().contains('Partially paid');
-      }
-      if (isFullyPaid) {
-        actions.getSummaryElement().contains('Fully paid');
-      }
-
-      const expectedDate = formatDate(invoiceDueDate);
-      actions.getSummaryElement().contains(expectedDate);
+    it('fail to get pdf image - return 500', () => {
+      actions.setRoutesNoPdfImg(500);
+      cy.visit(token);
+      // on 500 i expect 3 retries calls
+      actions.waitForPdfAndPdfImageRetries();
+      actions.verifyPdfError();
     });
   });
 });
