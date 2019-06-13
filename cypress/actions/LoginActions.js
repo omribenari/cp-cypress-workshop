fc7067307a0fff963586e63e1b9dbe422cd3bb0a
obenari <Omri_BenAri@intuit.com>
2019-06-13 15:30:46 +0300

    SAVEPOINT

--- cypress/actions/LoginActions.js
+++ cypress/actions/LoginActions.js
@@ -1,5 +1,14 @@
 export const LoginActions = {
-  checkIfSignedOut() {},
-  checkIfSignedIn(email) {},
-  siginOut() {}
+  checkIfSignedOut() {
+    cy.contains('header', 'Sign in');
+  },
+  checkIfSignedIn(email) {
+    return cy.get('header').contains(email);
+  },
+  signOut(email) {
+    this.checkIfSignedIn(email).click();
+    cy.get('.sign-out-menu')
+      .contains('Sign out')
+      .click();
+  }
 };
--- cypress/integration/Login.test.js
+++ cypress/integration/Login.test.js
@@ -1,5 +1,7 @@
 /// <reference types="Cypress" />
 
+import { LoginActions as Actions } from '../actions/LoginActions';
+
 const token =
   '50e78fb1bbd14641babf5eb37926d9f41a27783a2a0c42c7ba638f2c5cd15b79ea52b182aa714e20a4c9fd0acc762629';
 
@@ -8,24 +10,22 @@ const email = 'cypress1.iamtestpass@gmail.com';
 describe('Login tests', () => {
   it('should contain sign-in button', () => {
     cy.visit(token);
-    cy.contains('header', 'Sign in');
+    Actions.checkIfSignedOut();
   });
 
-  it('should be signed in', () => {
-    cy.login();
-    cy.visit(token);
-    cy.contains('header', email);
-  });
+  describe('User is signed in', () => {
+    beforeEach(() => {
+      cy.login();
+      cy.visit(token);
+    });
 
-  it.only('should signed out', () => {
-    cy.login();
-    cy.visit(token);
-    cy.get('header')
-      .contains(email)
-      .click();
-    cy.get('.sign-out-menu')
-      .contains('Sign out')
-      .click();
-    cy.contains('header', 'Sign in');
+    it('should be signed in', () => {
+      Actions.checkIfSignedIn(email);
+    });
+
+    it('should signed out', () => {
+      Actions.signOut(email);
+      Actions.checkIfSignedOut();
+    });
   });
 });
